52329311abbd32117a29d21124110fbb2d523113
cluniya <chandanluniya1986@gmail.com>
2024-03-13 10:22:11 +0530

    querySelector & querySelectorAll Methods_A6

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const listitem = document.getElementsByTagName('li');
-listitem[4].style.color = 'blue';
+const  mainHeading = document.querySelector('#main-heading');
+mainHeading.style.textAlign = 'center';
 
-for (let i = 0; i < listitem.length; i++) {
-    listitem[i].style.fontStyle = 'italic';
+
+
+const fruits = document.querySelector('.fruits');
+fruits.style.backgroundColor = 'gray';
+fruits.style.margin = '30px';
+fruits.style.padding = '30px';
+fruits.style.width = '40%';
+fruits.style.borderRadius = '5px';
+fruits.style.listStyleType = 'none';
+
+
+const basketHeading = document.querySelector('h2');
+basketHeading.style.color = 'brown';
+basketHeading.style.marginLeft = '30px';
+
+const fruit = document.querySelectorAll('.fruit');
+for (let i = 0; i < fruit.length; i++) {
+    fruit[i].style.backgroundColor = 'white';
+    fruit[i].style.margin = '10px';
+    fruit[i].style.padding = '10px';
+    fruit[i].style.borderRadius = '5px';
+}
+
+const evenfruit = document.querySelectorAll(".fruit:nth-child(even)");
+for (let i = 0; i < evenfruit.length; i++) {
+    evenfruit[i].style.backgroundColor = 'brown';
+    evenfruit[i].style.color = 'white';
     
 }

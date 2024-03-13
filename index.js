2ae5df8ce5b04dfa3bf22c1be9ccc88d0995b75e
cluniya <chandanluniya1986@gmail.com>
2024-03-13 10:15:46 +0530

    js added

---
+++ index.js
@@ -0,0 +1,6 @@
+const Fruit = document.getElementsByClassName('fruit');
+Fruit[2].style.backgroundColor = 'yellow';
+for (let i = 0; i < Fruit.length; i++) {
+    Fruit[i].style.fontWeight = 'bold';
+    
+}
